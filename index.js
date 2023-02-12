e05cd60a3d3f99bfeeecdc6e63be84b46da8b343
Lawal Ameh <amehlawal@gmail.com>
2023-02-12 19:03:54 +0100

    added image and created the start game function

--- index.js
+++ index.js
@@ -8,15 +8,21 @@ let isAlive = true;
 
 let message = "";
 
-if (sum < 21){
-    message= 'Do you want to draw a new card?😄';
-} else if (sum === 21){
-    message= "wohoo! You've got Blackjack!😄";
-    hasBlackJack = true;
-} else {
-    message= "You're out of the game!😄"
-    isAlive = false;
-}
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById('sum-el');
 
-// cash out 
-console.log(message);
+function startGame(){
+    sumEl.textContent='sum: ' + sum;
+    if (sum < 21){
+        message= 'Do you want to draw a new card?😄';
+    } else if (sum === 21){
+        message= "wohoo! You've got Blackjack!😄";
+        hasBlackJack = true;
+    } else {
+        message= "You're out of the game!😄"
+        isAlive = false;
+    }
+
+    // cash out 
+    messageEl.textContent = message;
+}
